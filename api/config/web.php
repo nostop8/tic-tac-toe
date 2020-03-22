0235eb9bca566d0298e72c57687787206a79f2b5
Volodymyr <volodymyr.dovbenko@gmail.com>
2020-03-22 16:45:17 +0200

    Nice URLs, CORS rules.

--- api/config/web.php
+++ api/config/web.php
@@ -13,6 +13,7 @@ $config = [
     ],
     'components' => [
         'request' => [
+            'baseUrl' => '/api',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'lSVfsZ20bDvHfDJTYQiZoZtzNKctIDVk',
         ],
@@ -43,14 +44,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
