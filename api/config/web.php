e241604a046c55b0d4842034ae7c014132649537
Volodymyr <volodymyr.dovbenko@gmail.com>
2020-03-23 23:39:06 +0200

    Branding.

--- api/config/web.php
+++ api/config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'Tic Tac Toe',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
